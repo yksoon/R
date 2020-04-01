a77f6a726a214b11b488c156efe00a3742565dd0
yksoon <ksyong1990@gmail.com>
2020-04-01 12:58:21 +0900

    2020_04_01_study3

--- 2020_04_01.R
+++ 2020_04_01.R
@@ -126,6 +126,139 @@ library(ggplot2)
 qplot(mid_new$asian_per_mean)
 
 
+##----------------------------------------------------------------------------------------------------##
+
+# 데이터 추출하기
+
+## 조건에 맞는 데이터만 추출하기
+exam %>% filter(class == 1)
+
+exam %>% filter(class != 1)
+
+exam %>% filter(math > 50)
+
+## 여러 조건을 충족하는 행 추출하기
+exam %>% filter(class == 1 & math >= 50)  # 1 반 이면서 수학 점수가 50 점 이상인 경우
+
+## 여러 조건 중 하나 이상 충족하는 행 추출하기
+exam %>% filter(math >= 90 | english >= 90)  # 수학 점수가 90 점 이상이거나 영어점수가 90 점 이상인 경우
+
+## %in% 기호 이용하기
+exam %>% filter(class %in% c(1,3,5))
+
+
+## 필요한 변수만 추출하기
+exam %>% select(math) # math 추출
+
+exam %>% select(class, math, english) # class, math, english 변수 추출
+
+exam %>% select(-math, -english) # math, english 제외
+
+
+
+# dplyr 함수 조합하기
+
+exam %>% filter(class == 1) %>% select(english)  # class 가 1 인 행만 추출한 다음 english 추출
+
+exam %>%
+  filter(class == 1) %>% # class 가 1 인 행 추출
+  select(english) # english 추출
+
+exam %>%
+  select(id, math) %>% # id, math 추출
+  head # 앞부분 6 행까지 추출
+
+
+##----------------------------------------------------------------------------##
+
+# 순서대로 정렬하기
+
+## 오름차순 정렬
+exam %>% arrange(math) # math 오름차순 정렬
+
+## 내림차순 정렬
+exam %>% arrange(desc(math)) # math 내림차순 정렬
+
+## 정렬 기준 변수 여러개 지정
+exam %>% arrange(class, math) # class 및 math 오름차순 정렬
+
+##---------------------------------------------------------------------------##
+
+# 파생변수 추가
+
+exam %>%
+  mutate(total = math + english + science) %>% # 총합 변수 추가
+  head # 일부 추출
+
+exam %>%
+  mutate(total = math + english + science, # 총합 변수 추가
+         mean = (math + english + science)/3) %>% # 총평균 변수 추가
+  head # 일부 추출
+
+
+## mutate()에 ifelse() 적용하기
+exam %>%
+  mutate(test = ifelse(science >= 60, "pass", "fail")) %>%
+  head
+
+## 추가한 변수를 dplyr 코드에 바로 활용하기
+exam %>%
+  mutate(total = math + english + science) %>% # 총합 변수 추가
+  arrange(total) %>% # 총합 변수 기준 정렬
+  head # 일부 추출
+
+##---------------------------------------------------------------------------##
+
+# 집단별로 요약하기
+
+exam %>% summarise(mean_math = mean(math)) # math 평균 산출
+
+exam %>%
+  group_by(class) %>% # class 별로 분리
+  summarise(mean_math = mean(math), # math 평균
+            sum_math = sum(math), # math 합계
+            median_math = median(math), # math 중앙값
+            n = n()) # 학생 수
+mpg %>%
+  group_by(manufacturer) %>% # 회사별로 분리
+  filter(class == "suv") %>% # suv 추출
+  mutate(tot = (cty+hwy)/2) %>% # 통합 연비 변수 생성
+  summarise(mean_tot = mean(tot)) %>% # 통합 연비 평균 산출
+  arrange(desc(mean_tot)) %>% # 내림차순 정렬
+  head(5) # 1~5 위까지 출력
+
+
+# 데이터 합치기
+
+# 중간고사 데이터 생성
+test1 <- data.frame(id = c(1, 2, 3, 4, 5),
+                    midterm = c(60, 80, 70, 90, 85))
+# 기말고사 데이터 생성
+test2 <- data.frame(id = c(1, 2, 3, 4, 5),
+                    final = c(70, 83, 65, 95, 80))
+
+total <- left_join(test1, test2, by = "id") # id 기준으로 합쳐 total 에 할당
+total # total 출력
+
+
+name <- data.frame(class = c(1, 2, 3, 4, 5),     # 반별 담임교사 명단 생성
+                   teacher = c("kim", "lee", "park", "choi", "jung"))
+name
+
+exam_new <- left_join(exam, name, by = "class")  # class기준 합치기
+exam_new
+
+## 세로로 합치기
+
+# 학생 1~5 번 시험 데이터 생성
+group_a <- data.frame(id = c(1, 2, 3, 4, 5),
+                      test = c(60, 80, 70, 90, 85))
+# 학생 6~10 번 시험 데이터 생성
+group_b <- data.frame(id = c(6, 7, 8, 9, 10),
+                      test = c(70, 83, 65, 95, 80))
+
+group_all <- bind_rows(group_a, group_b) # 데이터 합쳐서 group_all 에 할당
+group_all # group_all 출력
 
 
 
